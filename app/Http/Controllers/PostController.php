47705354b045ca304e01ded0e77b4254773d76e5
nucleusgenius2 <nucleusgenius@mail.ru>
2024-12-13 18:44:11 +0300

    refactoring: changing the structure to the CRUD pattern

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -415,4 +415,65 @@ class PostController
 
         return $this->responseJsonApi();
     }
+
+    /**
+     * @OA\delete(
+     *  path="/api/posts/{post_id}",
+     *  summary="Destroy post",
+     *  description="deleting a post by id",
+     *  operationId="postsDestroy",
+     *  security={{"sanctum":{}}},
+     *  tags={"post"},
+     *  @OA\Parameter(
+     *        description="post id",
+     *        in="path",
+     *        name="post_id",
+     *        required=true,
+     *        example="1",
+     *  ),
+     *  @OA\Response(
+     *    response=200,
+     *    description="Array of posts received",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="text", type="string"),
+     *       @OA\Property(property="status", type="string", example="success"),
+     *       @OA\Property(property="json", type="array", @OA\Items() )
+     *   ),
+     *  ),
+     *  @OA\Response(
+     *      response=422,
+     *      description="Unprocessable Entity",
+     *      @OA\JsonContent(
+     *         @OA\Property(property="text", type="string", example="Запрашиваемого ресурса не существует"),
+     *         @OA\Property(property="status", type="string", example="error"),
+     *         @OA\Property(property="json", type="null")
+     *       )
+     *  )
+     * )
+     */
+    public function destroy(int $id): JsonResponse
+    {
+        $validated = Validator::make(['id' => $id], [
+            'id' => 'required|integer|min:1',
+        ]);
+
+        if ($validated->fails()) {
+            $this->text = $validated->errors();
+        } else {
+            $data = $validated->valid();
+
+            $post = Post::where('id', '=', $data['id'])->delete();
+
+            Cache::forget('post_id_'.$data['id']);
+
+            if ($post) {
+                $this->status = 'success';
+                $this->code = 200;
+            } else {
+                $this->text = 'Запрашиваемого ресурса не существует';
+            }
+        }
+
+        return $this->responseJsonApi();
+    }
 }
